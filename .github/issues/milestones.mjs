16ad5c46fca6bffce708031fad0ed07ef6caef33
wfercanas <fernando.canas@outlook.com>
2023-10-15 16:09:54 -0500

    feat(github): get milestone id

---
+++ .github/issues/milestones.mjs
@@ -0,0 +1,18 @@
+function getMilestoneId(milestone) {
+  switch (milestone) {
+    case "prework":
+      return "4";
+    case "html-css":
+      return "5";
+    case "javascript":
+      return "6";
+    case "react":
+      return "7";
+    case "git":
+      return "8";
+    default:
+      throw new Error(`Milestone "${milestone}" not found`);
+  }
+}
+
+export { getMilestoneId };
