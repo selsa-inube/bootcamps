5677cd1399f4f651d221f3098e2565cf9aa9eb99
wfercanas <fernando.canas@outlook.com>
2023-10-15 21:13:36 -0500

    docs(issues): prework issues metadata

---
+++ content/prework/prework.mjs
@@ -0,0 +1,48 @@
+const URL = "https://github.com/selsa-inube/bootcamp/blob/main/content/";
+
+const prework = [
+  {
+    title: "Windows Subsystem for Linux",
+    body: `${URL}/prework/1-wsl.md`,
+    labels: ["lecture"],
+    milestone: "prework",
+  },
+  {
+    title: "Windows Terminal",
+    body: `${URL}/prework/2-terminal.md`,
+    labels: ["lecture"],
+    milestone: "prework",
+  },
+  {
+    title: "ZSH and Node",
+    body: `${URL}/prework/3-zsh-node.md`,
+    labels: ["lecture"],
+    milestone: "prework",
+  },
+  {
+    title: "VS Code",
+    body: `${URL}/prework/4-vscode.md`,
+    labels: ["lecture"],
+    milestone: "prework",
+  },
+  {
+    title: "Git",
+    body: `${URL}/prework/5-git.md`,
+    labels: ["lecture"],
+    milestone: "prework",
+  },
+  {
+    title: "Conventional Commits",
+    body: `${URL}/prework/6-conventional-commits.md`,
+    labels: ["lecture"],
+    milestone: "prework",
+  },
+  {
+    title: "Prettier",
+    body: `${URL}/prework/7-prettier.md`,
+    labels: ["lecture"],
+    milestone: "prework",
+  },
+];
+
+export { prework };
