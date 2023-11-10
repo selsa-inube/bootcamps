94a328e2f661dc13ae56e05a0fa378e13402e0f3
wfercanas <fernando.canas@outlook.com>
2023-11-10 17:28:40 -0500

    docs(js): meta info of js tasks

---
+++ content/meta/js.mjs
@@ -0,0 +1,221 @@
+const URL = "https://github.com/selsa-inube/bootcamp/blob/main/content/";
+
+const lectures = [
+  {
+    title: "L1 - Values, Types and Operators",
+    body: `${URL}/js/l1-values-types-operators.md`,
+    labels: ["lecture"],
+    milestone: "javascript",
+  },
+  {
+    title: "L2 - Program Structure",
+    body: `${URL}/js/l2-program-structure.md`,
+    labels: ["lecture"],
+    milestone: "javascript",
+  },
+  {
+    title: "L3 - Functions",
+    body: `${URL}/js/l3-functions.md`,
+    labels: ["lecture"],
+    milestone: "javascript",
+  },
+  {
+    title: "L4 - Data Structures",
+    body: `${URL}/js/l4-data-structures.md`,
+    labels: ["lecture"],
+    milestone: "javascript",
+  },
+  {
+    title: "L5 - Higher Order Functions",
+    body: `${URL}/js/l5-higher-order-functions.md`,
+    labels: ["lecture"],
+    milestone: "javascript",
+  },
+  {
+    title: "L6 - The Secret Life of Objects",
+    body: `${URL}/js/l6-the-secret-life-of-objects.md`,
+    labels: ["lecture"],
+    milestone: "javascript",
+  },
+  {
+    title: "L7 - Project: A Robot",
+    body: `${URL}/js/l7-project-a-robot.md`,
+    labels: ["lecture"],
+    milestone: "javascript",
+  },
+  {
+    title: "L8 - Bugs and Errors",
+    body: `${URL}/js/l8-bugs-and-errors.md`,
+    labels: ["lecture"],
+    milestone: "javascript",
+  },
+  {
+    title: "L9 - Modules",
+    body: `${URL}/js/l9-modules.md`,
+    labels: ["lecture"],
+    milestone: "javascript",
+  },
+  {
+    title: "L10 - JavaScript and the Browser",
+    body: `${URL}/js/l10-js-and-the-browser.md`,
+    labels: ["lecture"],
+    milestone: "javascript",
+  },
+  {
+    title: "L11 - Document Object Model",
+    body: `${URL}/js/l11-document-object-model.md`,
+    labels: ["lecture"],
+    milestone: "javascript",
+  },
+  {
+    title: "L12 - Handling Events",
+    body: `${URL}/js/l12-handling-events.md`,
+    labels: ["lecture"],
+    milestone: "javascript",
+  },
+  {
+    title: "L13 - HTTP and Forms",
+    body: `${URL}/js/l13-http-and-forms.md`,
+    labels: ["lecture"],
+    milestone: "javascript",
+  },
+];
+
+const activities = [
+  {
+    title: "A1 - EloquentJS - Chapter 2",
+    body: `${URL}/js/a1-eloquentjs-ch2.md`,
+    labels: ["assignment"],
+    milestone: "javascript",
+  },
+  {
+    title: "A2 - EloquentJS - Chapter 3",
+    body: `${URL}/js/a2-eloquentjs-ch3.md`,
+    labels: ["assignment"],
+    milestone: "javascript",
+  },
+  {
+    title: "A3 - EloquentJS - Chapter 4",
+    body: `${URL}/js/a3-eloquentjs-ch4.md`,
+    labels: ["assignment"],
+    milestone: "javascript",
+  },
+  {
+    title: "A4 - FreeCodeCamp - Basic JS",
+    body: `${URL}/js/a4-freecodecamp-basic-js.md`,
+    labels: ["exercise"],
+    milestone: "javascript",
+  },
+  {
+    title: "A5 - FreeCodeCamp - Basic Data Structures",
+    body: `${URL}/js/a5-freecodecamp-basic-data-structures.md`,
+    labels: ["exercise"],
+    milestone: "javascript",
+  },
+  {
+    title: "A6 - EloquentJS - Chapter 5",
+    body: `${URL}/js/a6-eloquentjs-ch5.md`,
+    labels: ["assignment"],
+    milestone: "javascript",
+  },
+  {
+    title: "A7 - EloquentJS - Chapter 6",
+    body: `${URL}/js/a7-eloquentjs-ch6.md`,
+    labels: ["assignment"],
+    milestone: "javascript",
+  },
+  {
+    title: "A8 - EloquentJS - Chapter 7",
+    body: `${URL}/js/a8-eloquentjs-ch7.md`,
+    labels: ["assignment"],
+    milestone: "javascript",
+  },
+  {
+    title: "A9 - FreeCodeCamp - ES6",
+    body: `${URL}/js/a9-freecodecamp-es6.md`,
+    labels: ["exercise"],
+    milestone: "javascript",
+  },
+  {
+    title: "A10 - FreeCodeCamp - Debugging",
+    body: `${URL}/js/a10-freecodecamp-debugging.md`,
+    labels: ["exercise"],
+    milestone: "javascript",
+  },
+  {
+    title: "A11 - FreeCodeCamp - Basic Algorithm Scripting",
+    body: `${URL}/js/a11-freecodecamp-basic-algorithm-scripting.md`,
+    labels: ["exercise"],
+    milestone: "javascript",
+  },
+  {
+    title: "A12 - FreeCodeCamp - Object Oriented Programming",
+    body: `${URL}/js/a12-freecodecamp-oop.md`,
+    labels: ["exercise"],
+    milestone: "javascript",
+  },
+  {
+    title: "A13 - FreeCodeCamp - Functional Programming",
+    body: `${URL}/js/a13-freecodecamp-functional-programming.md`,
+    labels: ["exercise"],
+    milestone: "javascript",
+  },
+  {
+    title: "A14 - FreeCodeCamp - Intermediate Algorithm Scripting",
+    body: `${URL}/js/a14-freecodecamp-intermediate-algorithm-scripting.md`,
+    labels: ["exercise"],
+    milestone: "javascript",
+  },
+  {
+    title: "A15 - FreeCodeCamp - Regular Expressions",
+    body: `${URL}/js/a15-freecodecamp-regexp.md`,
+    labels: ["exercise"],
+    milestone: "javascript",
+  },
+  {
+    title: "A16 - FreeCodeCamp - Projects",
+    body: `${URL}/js/a16-freecodecamp-projects.md`,
+    labels: ["exercise"],
+    milestone: "javascript",
+  },
+  {
+    title: "A17 - EloquentJS - Chapter 8",
+    body: `${URL}/js/a17-eloquentjs-ch8.md`,
+    labels: ["assignment"],
+    milestone: "javascript",
+  },
+  {
+    title: "A18 - EloquentJS - Chapter 10",
+    body: `${URL}/js/a18-eloquentjs-ch10.md`,
+    labels: ["assignment"],
+    milestone: "javascript",
+  },
+  {
+    title: "A19 - EloquentJS - Chapter 13",
+    body: `${URL}/js/a19-eloquentjs-ch13.md`,
+    labels: ["assignment"],
+    milestone: "javascript",
+  },
+  {
+    title: "A20 - EloquentJS - Chapter 14",
+    body: `${URL}/js/a20-eloquentjs-ch14.md`,
+    labels: ["assignment"],
+    milestone: "javascript",
+  },
+  {
+    title: "A21 - EloquentJS - Chapter 15",
+    body: `${URL}/js/a21-eloquentjs-ch15.md`,
+    labels: ["assignment"],
+    milestone: "javascript",
+  },
+  {
+    title: "A22 - EloquentJS - Chapter 18",
+    body: `${URL}/js/a22-eloquentjs-ch18.md`,
+    labels: ["assignment"],
+    milestone: "javascript",
+  },
+];
+
+const js = [...lectures, ...activities];
+
+export { js };
