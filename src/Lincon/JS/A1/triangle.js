3e1dd4bae3073564b5a1c30038eab06666876993
lincon ossa <ossalincon422@gmail.com>
2023-11-20 08:43:08 -0500

    feat: creating exercises

---
+++ src/Lincon/JS/A1/triangle.js
@@ -0,0 +1,8 @@
+// Looping a triangle
+console.log("----Looping a triangle----");
+let numeral = "#";
+
+for (let lista = 0; lista < 7; lista += 1) {
+  console.log(numeral);
+  numeral = numeral + "#";
+}
