8f604690ec7ec0b5d2c5ae5915e649c3edc70432
Januar Asprilla <ajanuarandres@gmail.com>
2023-11-23 11:36:10 -0500

    feat: finalizacion de la actividad A1 de JS

--- src/januar_asprilla/JS/A1.js
+++ src/januar_asprilla/JS/A1.js
@@ -1,39 +1,41 @@
+console.log("//Looping a triangle//");
 
-console.log("//Looping a triangle//"){
-    for (let i = 1; i <= 7; i++) {
-        let asterisco = '';
-        for (let a = 1; a = 7; a++) {
-            asterisco += '#';
-        }
-        console.log(asterisco);
-    }
+for (let i = 1; i <= 7; i++) {
+  let asterisco = "";
+  for (let a = 1; a <= i; a++) {
+    asterisco += "#";
+  }
+  console.log(asterisco);
 }
-console.log("//FizzBuzz//")
+
+console.log("//FizzBuzz//");
 
 for (let i = 1; i <= 100; i++) {
-    if (i % 3 === 0 && i % 5 === 0) {
-        console.log("FizzBuzz");
-    } else if (i % 3 === 0) {
-        console.log("Fizz");
-    } else if (i % 5 === 0) {
-        console.log("Buzz");
+  if (i % 3 === 0 && i % 5 === 0) {
+    console.log("FizzBuzz");
+  } else if (i % 3 === 0) {
+    console.log("Fizz");
+  } else if (i % 5 === 0) {
+    console.log("Buzz");
+  } else {
+    console.log(i);
+  }
+}
+
+console.log("//Chessboard//");
+
+let tamaño = 8;
+let unidad_ta = "";
+
+for (let i = 0; i < tamaño; i++) {
+  for (let j = 0; j < tamaño; j++) {
+    if ((i + j) % 2 === 0) {
+      unidad_ta += " ";
     } else {
-        console.log(i);
+      unidad_ta += "#";
     }
+  }
+  unidad_ta += "\n";
 }
-console.log("//Chessboard//")
 
-
-
-for (let i = 1; i <= 8; i++){
-    let  asteristo = '';
-    let espacios = '';
-    for (let b = 1; b <= 8; b++) {
-        asteristo += '#';
-        espacios += '';
-        for (let c = 1; c <= 8; c++) {
-            espacios += '';
-            asteristo += '#';
-        }
-    }
-}
+console.log(unidad_ta);
