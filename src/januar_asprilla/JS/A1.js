74aefeaf0fb059066bc957709e553bcdb3993757
Januar Asprilla <ajanuarandres@gmail.com>
2023-11-24 08:48:47 -0500

    refactor: refactorizacion del segundo ejercicio, ademas se corrigen los nombres de las variables

--- src/januar_asprilla/JS/A1.js
+++ src/januar_asprilla/JS/A1.js
@@ -1,41 +1,45 @@
 console.log("//Looping a triangle//");
 
 for (let i = 1; i <= 7; i++) {
-  let asterisco = "";
+  let row = "";
   for (let a = 1; a <= i; a++) {
-    asterisco += "#";
+    row += "#";
   }
-  console.log(asterisco);
+  console.log(row);
 }
 
 console.log("//FizzBuzz//");
 
 for (let i = 1; i <= 100; i++) {
-  if (i % 3 === 0 && i % 5 === 0) {
-    console.log("FizzBuzz");
-  } else if (i % 3 === 0) {
-    console.log("Fizz");
-  } else if (i % 5 === 0) {
-    console.log("Buzz");
-  } else {
-    console.log(i);
+  switch (true) {
+    case i % 3 === 0 && i % 5 === 0:
+      console.log("FizzBuzz");
+      break;
+    case i % 3 === 0:
+      console.log("Fizz");
+      break;
+    case i % 5 === 0:
+      console.log("Buzz");
+      break;
+    default:
+      console.log(i);
   }
 }
 
 console.log("//Chessboard//");
 
-let tamaño = 8;
-let unidad_ta = "";
+let size = 8;
+let unit_size = "";
 
-for (let i = 0; i < tamaño; i++) {
-  for (let j = 0; j < tamaño; j++) {
+for (let i = 0; i < size; i++) {
+  for (let j = 0; j < size; j++) {
     if ((i + j) % 2 === 0) {
-      unidad_ta += " ";
+      unit_size += " ";
     } else {
-      unidad_ta += "#";
+      unit_size += "#";
     }
   }
-  unidad_ta += "\n";
+  unit_size += "\n";
 }
 
-console.log(unidad_ta);
+console.log(unit_size);
