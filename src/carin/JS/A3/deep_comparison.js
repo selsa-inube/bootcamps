dfb1a6c85248e23d3d6265cf684239401c514bae
carin <ca.rincon97@gmail.com>
2023-11-29 16:41:41 -0500

    refactor: refactoring deep_comparison

--- src/carin/JS/A3/deep_comparison.js
+++ src/carin/JS/A3/deep_comparison.js
@@ -2,12 +2,13 @@ console.log("###############Deep comparison########");
 
 const deepEqual = (element1, element2) => {
   let result = true;
-  if (
-    typeof element1 == typeof element2 &&
-    typeof element1 == "object" &&
-    element1 &&
-    element2
-  ) {
+  let flag = true;
+  if (!(typeof element1 == typeof element2)) flag = false;
+  if (!(typeof element1 == "object")) flag = false;
+  if (!element1) flag = false;
+  if (!element2) flag = false;
+
+  if (flag) {
     Object.keys(element1).forEach((element) => {
       if (!deepEqual(element1[element], element2[element])) {
         result = false;
@@ -16,6 +17,7 @@ const deepEqual = (element1, element2) => {
     });
   } else {
     result = element1 === element2;
+    return element1 === element2;
   }
   return result;
 };
@@ -27,6 +29,6 @@ console.log(deepEqual(obj, { here: 1, object: 2 }));
 // → false
 console.log(deepEqual(obj, { here: { is: "an" }, object: 2 }));
 // → true
-console.log(deepEqual(2, 2));
+console.log(deepEqual(2, 3));
 console.log(deepEqual([1, 2], [1, 2]));
 console.log(deepEqual(null, null));
