36e28225c8bd2fcc1324ce8cc9018fec3f585101
Januar Asprilla <ajanuarandres@gmail.com>
2023-12-06 16:18:38 -0500

    fix: correction completed

--- src/januar_asprilla/JS/A8/persistent-group.js
+++ src/januar_asprilla/JS/A8/persistent-group.js
@@ -9,32 +9,32 @@ class PGroup {
     if (!this.has(value)) {
       return new PGroup([...this.members, value]);
     }
-    return this;
+    return new PGroup([...this.members]); // Devuelve una nueva instancia en lugar de la referencia actual
   }
 
   delete(value) {
     if (this.has(value)) {
       return new PGroup(this.members.filter((member) => member !== value));
     }
-    return this;
+    return new PGroup([...this.members]); // Devuelva una nueva instancia en lugar de la referencia actual
   }
 
   has(value) {
     return this.members.includes(value);
   }
 
-  static get empty() {
-    if (!PGroup._empty) {
-      PGroup._empty = new PGroup();
-    }
-    return PGroup._empty;
+  static empty() {
+    return new PGroup(); // Retorna una nueva instancia en lugar de usar una propiedad estática
   }
 }
 
-let a = PGroup.empty.add("a");
-let ab = a.add("b");
-let b = ab.delete("a");
+let a = PGroup.empty().add("a");
+let b = a.add("a");
+console.log(a === b); // false
 
-console.log(b.has("b")); // → true
+let ab = a.add("b");
+let c = ab.delete("a");
+
+console.log(c.has("b")); // → true
 console.log(a.has("b")); // → false
-console.log(b.has("a")); // → false
+console.log(c.has("a")); // → false
