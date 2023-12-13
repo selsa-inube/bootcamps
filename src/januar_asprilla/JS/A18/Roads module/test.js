7e239a7ea7b94c80084ee5688eea69de5e238a43
Januar Asprilla <ajanuarandres@gmail.com>
2023-12-13 08:52:17 -0500

    feat: activity  completed

---
+++ src/januar_asprilla/JS/A18/Roads module/test.js
@@ -0,0 +1,5 @@
+// Importando el grafo de carreteras desde el módulo "roads"
+const { roadGraph } = require("../Roads module/roads");
+
+// Aqui se muestran las carreteras del grafo
+console.log(roadGraph);
