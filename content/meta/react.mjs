81b817cdc1e5443c1ad1c5f447fa688f415abdb7
wfercanas <fernando.canas@outlook.com>
2023-12-18 20:46:25 -0500

    docs(meta): react issues

---
+++ content/meta/react.mjs
@@ -0,0 +1,141 @@
+const URL = "https://github.com/selsa-inube/bootcamp/blob/main/content/";
+
+const lectures = [
+  {
+    title: "L1 - Your First Component",
+    body: `${URL}/react/l1-your-first-component.md`,
+    labels: ["lecture"],
+    milestone: "react",
+  },
+  {
+    title: "L2 - Importing and Exporting Components",
+    body: `${URL}/react/l2-importing-and-exporting-components.md`,
+    labels: ["lecture"],
+    milestone: "react",
+  },
+  {
+    title: "L3 - Writing Markup with JSX",
+    body: `${URL}/react/l3-writing-markup-with-jsx.md`,
+    labels: ["lecture"],
+    milestone: "react",
+  },
+  {
+    title: "L4 - JavaScript in JSX with Curly Braces",
+    body: `${URL}/react/l4-js-in-jsx-with-curly-braces.md`,
+    labels: ["lecture"],
+    milestone: "react",
+  },
+  {
+    title: "L5 - Passing Props to a Component",
+    body: `${URL}/react/l5-passing-props-to-a-component.md`,
+    labels: ["lecture"],
+    milestone: "react",
+  },
+  {
+    title: "L6 - Conditional Rendering",
+    body: `${URL}/react/l6-conditional-rendering.md`,
+    labels: ["lecture"],
+    milestone: "react",
+  },
+  {
+    title: "L7 - Rendering Lists",
+    body: `${URL}/react/l7-rendering-lists.md`,
+    labels: ["lecture"],
+    milestone: "react",
+  },
+  {
+    title: "L8 - Keeping Components Pure",
+    body: `${URL}/react/l8-keeping-components-pure.md`,
+    labels: ["lecture"],
+    milestone: "react",
+  },
+  {
+    title: "L9 - Understanding your UI as a Tree",
+    body: `${URL}/react/l9-understanding-your-ui-as-a-tree.md`,
+    labels: ["lecture"],
+    milestone: "react",
+  },
+  {
+    title: "L10 - Responding to Events",
+    body: `${URL}/react/l10-responding-to-events.md`,
+    labels: ["lecture"],
+    milestone: "react",
+  },
+  {
+    title: "L11 - State: a Component's Memory ",
+    body: `${URL}/react/l11-state-a-components-memory.md`,
+    labels: ["lecture"],
+    milestone: "react",
+  },
+  {
+    title: "L12 - Render and Commit",
+    body: `${URL}/react/l12-render-and-commit.md`,
+    labels: ["lecture"],
+    milestone: "react",
+  },
+  {
+    title: "L13 - State as a Snapshot",
+    body: `${URL}/react/l13-state-as-a-snapshot.md`,
+    labels: ["lecture"],
+    milestone: "react",
+  },
+  {
+    title: "L14 - Queuing a Series of State Updates",
+    body: `${URL}/react/l14-queuing-a-series-of-state-updates.md`,
+    labels: ["lecture"],
+    milestone: "react",
+  },
+  {
+    title: "L15 - Updating Objects in State",
+    body: `${URL}/react/l15-updating-objects-in-state.md`,
+    labels: ["lecture"],
+    milestone: "react",
+  },
+  {
+    title: "L16 - Updating Arrays in State",
+    body: `${URL}/react/l16-updating-arrays-in-state.md`,
+    labels: ["lecture"],
+    milestone: "react",
+  },
+];
+
+const activities = [
+  {
+    title: "A1 - Google in React",
+    body: `${URL}/react/a1-google-in-react.md`,
+    labels: ["assignment"],
+    milestone: "react",
+  },
+  {
+    title: "A2 - New York Times in React",
+    body: `${URL}/react/a2-new-york-times-in-react.md`,
+    labels: ["assignment"],
+    milestone: "react",
+  },
+  {
+    title: "A3 - Cards",
+    body: `${URL}/react/a3-cards.md`,
+    labels: ["assignment"],
+    milestone: "react",
+  },
+  {
+    title: "A4 - Tic Tac Toe",
+    body: `${URL}/react/a4-tic-tac-toe.md`,
+    labels: ["assignment"],
+    milestone: "react",
+  },
+  {
+    title: "A5 - Thinking in React",
+    body: `${URL}/react/a5-thinking-in-react.md`,
+    labels: ["assignment"],
+    milestone: "react",
+  },
+  {
+    title: "A6 - Chat",
+    body: `${URL}/react/a6-chat.md`,
+    labels: ["assignment"],
+    milestone: "react",
+  },
+];
+
+export const react = [...lectures, ...activities];
