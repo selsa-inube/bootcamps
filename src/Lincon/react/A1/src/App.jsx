055b7c0700ee6bbf6f01194b0a0ed07e69762265
lincon ossa <ossalincon422@gmail.com>
2023-12-22 12:01:08 -0500

    feat: creating exercises

--- src/Lincon/react/A1/src/App.jsx
+++ src/Lincon/react/A1/src/App.jsx
@@ -1,34 +1,88 @@
 import { useState } from "react";
-import reactCarnet from "./assets/carnet.jpeg";
+import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
-import "./App.css";
-import { Button, Container } from "./styles";
+import { Container, Container2, Button, ContainerDatos } from "./styles";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
-      <Container>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} id="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactCarnet} className="react" id="logo" alt="React logo" />
-        </a>
-      </Container>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <Button onClick={() => setCount((count) => count + 1)}>
-          La cuenta es: {count}
-        </Button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <body>
+        <header>
+          <ul>
+            <li>
+              <a href="https://mail.google.com">Gmail</a>
+            </li>
+            <li>
+              <a href="https://www.google.com/imghp?hl=en&authuser=0&ogbl">
+                Imágenes
+              </a>
+            </li>
+          </ul>
+          <img src="ima/menu.png" alt="" />
+          <img src="ima/perfil.jpeg" alt="perfil" />
+        </header>
+        <section>
+          <img src="ima/google.png" alt="google" />
+          <Container>
+            <i className="fa fa-search icon-left"></i>
+            <input type="text" id="search-input" />
+            <img src="ima/camara.png" className="icon-right" alt="" />
+          </Container>
+
+          <div className="botones">
+            <Button>Buscar con Google</Button>
+            <Button>Voy a tener suerte</Button>
+          </div>
+        </section>
+        <footer>
+          <div className="colombia">
+            <p>Colombia</p>
+          </div>
+          <Container2>
+            <ContainerDatos>
+              <ul>
+                <li>
+                  <a href="https://about.google/?utm_source=google-CO&utm_medium=referral&utm_campaign=hp-footer&fg=1">
+                    Sobre Google
+                  </a>
+                </li>
+                <li>
+                  <a href="https://ads.google.com/intl/en_co/home/?subid=ww-ww-et-g-awa-a-g_hpafoot1_1!o2&utm_source=google.com&utm_medium=referral&utm_campaign=google_hpafooter&fg=1">
+                    Publicidad
+                  </a>
+                </li>
+                <li>
+                  <a href="https://smallbusiness.withgoogle.com/intl/es-419_co/?subid=ww-ww-et-g-awa-a-g_hpbfoot1_1!o2&utm_source=google&utm_medium=ep&utm_campaign=google_hpbfooter&utm_content=google_hpbfooter&gmbsrc=ww-ww-et-gs-z-gmb-s-z-u~sb-g4sb_srvcs-u&c=CO#!/">
+                    Negocios
+                  </a>
+                </li>
+                <li>
+                  <a href="https://www.google.com/search/howsearchworks/?fg=1">
+                    Como funciona la Busqueda
+                  </a>
+                </li>
+              </ul>
+            </ContainerDatos>
+            <div className="datos-2">
+              <ul>
+                <li>
+                  <a href="https://policies.google.com/privacy?hl=en-CO&fg=1">
+                    Privacidad
+                  </a>
+                </li>
+                <li>
+                  <a href="https://policies.google.com/terms?hl=en-CO&fg=1">
+                    Condiciones
+                  </a>
+                </li>
+                <li>
+                  <a href="#">Preferencias</a>
+                </li>
+              </ul>
+            </div>
+          </Container2>
+        </footer>
+      </body>
     </>
   );
 }
--- src/Lincon/react/A1/src/styles.js
+++ src/Lincon/react/A1/src/styles.js
@@ -1,27 +1,44 @@
 import styled from "styled-components";
 
-const Button = styled.button`
-  border-radius: 8px;
-  border: 1px solid transparent;
-  padding: 0.6em 1.2em;
-  font-size: 1em;
-  font-weight: 500;
-  font-family: inherit;
-  background-color: #1a1a1a;
-  cursor: pointer;
-  transition: border-color 0.25s;
-
-  :hover {
-    border-color: #646cff;
-  }
-  :focus,
-  :focus-visible {
-    outline: 4px auto -webkit-focus-ring-color;
-  }
-`;
 const Container = styled.div`
   display: flex;
-  flex-direction: column;
-  gap: 50px;
+  align-items: center;
+  width: 90%;
+  max-width: 700px;
+  position: relative;
 `;
-export { Button, Container };
+const Container2 = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  flex-wrap: wrap;
+  background-color: #e0d9d9;
+  padding: 10px 30px;
+  gap: 20px;
+`;
+const Button = styled.button`
+  border: none;
+  color: #505050;
+  font-size: 15px;
+  padding: 15px 20px;
+  cursor: pointer;
+  border-radius: 6px;
+`;
+const ContainerDatos = styled.div`
+  ul {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-wrap: wrap;
+    list-style: none;
+    gap: 20px;
+  }
+  li a {
+    color: #505050;
+    text-decoration: none;
+  }
+  li a:hover {
+    border-bottom: 1px solid #919191;
+  }
+`;
+export { Container, Container2, Button, ContainerDatos };
