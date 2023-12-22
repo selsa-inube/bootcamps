c6fd4056b42edaea2631b6bd86b6d985e38dfb4e
Januar Asprilla <ajanuarandres@gmail.com>
2023-12-22 08:55:04 -0500

    feat: correction complete

--- src/januar_asprilla/JS/A20/Elements by tag name/assets/js/main.js
+++ src/januar_asprilla/JS/A20/Elements by tag name/assets/js/main.js
@@ -1,8 +1,28 @@
-// Definimos una función llamada byTagName que toma un nodo y un nombre de etiqueta como argumentos
 function byTagName(node, tagName) {
-  // Utilizamos getElementsByTagName en el nodo proporcionado para obtener todos los elementos con la etiqueta especificada
-  // Luego, utilizamos Array.from para convertir la NodeList resultante en un array de elementos
-  return Array.from(node.getElementsByTagName(tagName));
+  // Inicializamos un array para almacenar los elementos encontrados
+  let elements = [];
+
+  // Función recursiva para buscar elementos por etiqueta
+  function searchElements(currentNode) {
+    // Verificamos si el nodo actual es un elemento y tiene la etiqueta especificada
+    if (
+      currentNode.nodeType === 1 &&
+      currentNode.tagName.toLowerCase() === tagName.toLowerCase()
+    ) {
+      elements.push(currentNode);
+    }
+
+    // Recorremos los nodos hijos del nodo actual
+    for (let childNode of currentNode.childNodes) {
+      // Llamada recursiva para procesar nodos hijos
+      searchElements(childNode);
+    }
+  }
+
+  // Iniciamos la búsqueda desde el nodo proporcionado
+  searchElements(node);
+
+  return elements;
 }
 
 console.log(byTagName(document.body, "h1").length);
