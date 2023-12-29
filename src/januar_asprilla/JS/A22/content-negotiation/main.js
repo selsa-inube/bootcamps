665a80ab1080397f9c5ff27e05ca0ad484d662dc
Januar Asprilla <ajanuarandres@gmail.com>
2023-12-28 20:29:41 -0500

    fix: correction complete

--- src/januar_asprilla/JS/A22/content-negotiation/main.js
+++ src/januar_asprilla/JS/A22/content-negotiation/main.js
@@ -7,15 +7,19 @@ const types = [
 ];
 async function showTypes() {
   try {
-    const responses = await Promise.all(
-      types.map(async (type) => {
-        // Realiza una solicitud fetch para cada tipo de medio
-        const resp = await fetch(url, { headers: { accept: type } });
+    const fetchAndFormat = async (type) => {
+      // Realiza una solicitud fetch para un tipo de medio específico
+      const resp = await fetch(url, { headers: { accept: type } });
 
-        // Formatea la respuesta como una cadena y la devuelve
-        return `${type}: ${await resp.text()}\n`;
-      })
-    );
+      // Formatea la respuesta como una cadena y la devuelve
+      return `${type}: ${await resp.text()}\n`;
+    };
+
+    const responses = [];
+    for (const type of types) {
+      const response = await fetchAndFormat(type);
+      responses.push(response);
+    }
 
     console.log(responses.join(""));
   } catch (error) {
