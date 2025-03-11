81ca6550a484a2f2ec66dedebdd71bb3a897b3c1
Fernando Cañas <fernando.canas@outlook.com>
2025-03-11 12:33:23 -0500

    chore: change milestones for labels in prework

--- content/meta/prework.mjs
+++ content/meta/prework.mjs
@@ -4,50 +4,42 @@ const prework = [
   {
     title: "Windows Subsystem for Linux",
     body: `${URL}/prework/l1-wsl.md`,
-    labels: ["lecture"],
-    milestone: "prework",
+    labels: ["lecture", "topic: prework"],
   },
   {
     title: "Windows Terminal",
     body: `${URL}/prework/l2-terminal.md`,
-    labels: ["lecture"],
-    milestone: "prework",
+    labels: ["lecture", "topic: prework"],
   },
   {
     title: "ZSH and Node",
     body: `${URL}/prework/l3-zsh-node.md`,
-    labels: ["lecture"],
-    milestone: "prework",
+    labels: ["lecture", "topic: prework"],
   },
   {
     title: "VS Code",
     body: `${URL}/prework/l4-vscode.md`,
-    labels: ["lecture"],
-    milestone: "prework",
+    labels: ["lecture", "topic: prework"],
   },
   {
     title: "Git",
     body: `${URL}/prework/l5-git.md`,
-    labels: ["lecture"],
-    milestone: "prework",
+    labels: ["lecture", "topic: prework"],
   },
   {
     title: "Conventional Commits",
     body: `${URL}/prework/l6-conventional-commits.md`,
-    labels: ["lecture"],
-    milestone: "prework",
+    labels: ["lecture", "topic: prework"],
   },
   {
     title: "Prettier",
     body: `${URL}/prework/l7-prettier.md`,
-    labels: ["lecture"],
-    milestone: "prework",
+    labels: ["lecture", "topic: prework"],
   },
   {
     title: "Github",
     body: `${URL}/prework/l8-github.md`,
-    labels: ["lecture"],
-    milestone: "prework",
+    labels: ["lecture", "topic: prework"],
   },
 ];
 
