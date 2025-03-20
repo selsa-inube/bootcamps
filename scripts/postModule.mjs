06e0f8c4a23cf507b1bb38126f73a0d058ef64b9
Fernando Cañas <fernando.canas@outlook.com>
2025-03-19 21:36:47 -0500

    chore: importa go and sql modules in script

--- scripts/postModule.mjs
+++ scripts/postModule.mjs
@@ -5,13 +5,15 @@ import { createIssue } from "../.github/issues/createIssue.mjs";
 import { assignees } from "../.github/issues/assignees.mjs";
 
 // Modules
-import { prework } from "../content/meta/prework.mjs";
-// import { htmlCSS } from "../content/meta/html-css.mjs";
-// import { js } from "../content/meta/js.mjs";
-// import { react } from "../content/meta/react.mjs";
-// import { ts } from "../content/meta/ts.mjs";
+// import { prework } from "../content/frontend/meta/prework.mjs";
+// import { htmlCSS } from "../content/frontend/meta/html-css.mjs";
+// import { js } from "../content/frontend/meta/js.mjs";
+// import { react } from "../content/frontend/meta/react.mjs";
+// import { ts } from "../content/frontend/meta/ts.mjs";
+// import { go } from "../content/backend/meta/go.mjs";
+import { sql } from "../content/backend/meta/sql.mjs";
 
-const issues = prework;
+const issues = sql;
 const assignee = assignees["jramirez"];
 
 issues.forEach(({ title, body, labels }) => {
