4a554f2bcad5629f31116165d773fc28b4754838
Fernando Cañas <fernando.canas@outlook.com>
2025-04-22 10:22:42 -0500

    docs: meta for go backend v2

---
+++ content/backend/v2/meta/go.mjs
@@ -0,0 +1,44 @@
+const URL =
+  "https://github.com/selsa-inube/bootcamp/blob/main/content/backend/v2";
+
+const activities = [
+  {
+    title: "go-ex1-tour-welcome",
+    body: `${URL}/go/go-ex1-tour-welcome.md`,
+    labels: ["topic: go", "type: exercise"],
+  },
+  {
+    title: "go-ex2-tour-packages-variables-functions",
+    body: `${URL}/go/go-ex2-tour-packages-variables-functions.md`,
+    labels: ["topic: go", "type: exercise"],
+  },
+  {
+    title: "go-ex3-tour-flow-control-statements",
+    body: `${URL}/go/go-ex3-tour-flow-control-statements.md`,
+    labels: ["topic: go", "type: exercise"],
+  },
+  {
+    title: "go-ex4-tour-more-types",
+    body: `${URL}/go/go-ex4-tour-more-types.md`,
+    labels: ["topic: go", "type: exercise"],
+  },
+  {
+    title: "go-ex5-tour-methods-and-interfaces",
+    body: `${URL}/go/go-ex5-tour-methods-and-interfaces.md`,
+    labels: ["topic: go", "type: exercise"],
+  },
+  {
+    title: "go-ex6-tour-generics",
+    body: `${URL}/go/go-ex6-tour-generics.md`,
+    labels: ["topic: go", "type: exercise"],
+  },
+  {
+    title: "go-ex7-tour-concurrency",
+    body: `${URL}/go/go-ex7-tour-concurrency.md`,
+    labels: ["topic: go", "type: exercise"],
+  },
+];
+
+const go = [...activities];
+
+export { go };
--- scripts/postModule.mjs
+++ scripts/postModule.mjs
@@ -5,15 +5,18 @@ import { createIssue } from "../.github/issues/createIssue.mjs";
 import { assignees } from "../.github/issues/assignees.mjs";
 
 // Modules
-import { prework } from "../content/frontend/meta/prework.mjs";
-// import { htmlCSS } from "../content/frontend/meta/html-css.mjs";
-// import { js } from "../content/frontend/meta/js.mjs";
-// import { react } from "../content/frontend/meta/react.mjs";
-// import { ts } from "../content/frontend/meta/ts.mjs";
-// import { go } from "../content/backend/meta/go.mjs";
-import { sql } from "../content/backend/meta/sql.mjs";
 
-const issues = prework;
+// Frontend
+import { prework } from "../content/frontend/meta/prework.mjs";
+import { htmlCSS } from "../content/frontend/meta/html-css.mjs";
+import { js } from "../content/frontend/meta/js.mjs";
+import { react } from "../content/frontend/meta/react.mjs";
+import { ts } from "../content/frontend/meta/ts.mjs";
+
+// Backend
+import { go } from "../content/backend/v2/meta/go.mjs";
+
+const issues = go;
 const assignee = assignees.jpinilla;
 
 issues.forEach(({ title, body, labels }) => {
