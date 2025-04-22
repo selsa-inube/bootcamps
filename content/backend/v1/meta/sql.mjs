01c9526d175ca3f75e9d6d0e97ef590b5384b446
Fernando Cañas <fernando.canas@outlook.com>
2025-04-22 10:13:49 -0500

    chore: update url for backend v1 content

--- content/backend/v1/meta/sql.mjs
+++ content/backend/v1/meta/sql.mjs
@@ -3,42 +3,42 @@ const URL = "https://github.com/selsa-inube/bootcamp/blob/main/content";
 const activities = [
   {
     title: "as1-codecademy-sql-manipulation",
-    body: `${URL}/backend/sql/as1-codecademy-sql-manipulation.md`,
+    body: `${URL}/sql/as1-codecademy-sql-manipulation.md`,
     labels: ["topic: sql"],
   },
   {
     title: "as2-codecademy-sql-queries",
-    body: `${URL}/backend/sql/as2-codecademy-sql-queries.md`,
+    body: `${URL}/sql/as2-codecademy-sql-queries.md`,
     labels: ["topic: sql"],
   },
   {
     title: "as3-codecademy-sql-aggregate-functions",
-    body: `${URL}/backend/sql/as3-codecademy-sql-aggregate-functions.md`,
+    body: `${URL}/sql/as3-codecademy-sql-aggregate-functions.md`,
     labels: ["topic: sql"],
   },
   {
     title: "as4-codecademy-sql-multiple-tables",
-    body: `${URL}/backend/sql/as4-codecademy-sql-multiple-tables.md`,
+    body: `${URL}/sql/as4-codecademy-sql-multiple-tables.md`,
     labels: ["topic: sql"],
   },
   {
     title: "ls1-codecademy-sql-manipulation",
-    body: `${URL}/backend/sql/ls1-codecademy-sql-manipulation.md`,
+    body: `${URL}/sql/ls1-codecademy-sql-manipulation.md`,
     labels: ["topic: sql"],
   },
   {
     title: "ls2-codecademy-sql-queries",
-    body: `${URL}/backend/sql/ls2-codecademy-sql-queries.md`,
+    body: `${URL}/sql/ls2-codecademy-sql-queries.md`,
     labels: ["topic: sql"],
   },
   {
     title: "ls3-codecademy-sql-aggregate-functions",
-    body: `${URL}/backend/sql/ls3-codecademy-sql-aggregate-functions.md`,
+    body: `${URL}/sql/ls3-codecademy-sql-aggregate-functions.md`,
     labels: ["topic: sql"],
   },
   {
     title: "ls4-codecademy-sql-multiple-tables",
-    body: `${URL}/backend/sql/ls4-codecademy-sql-multiple-tables.md`,
+    body: `${URL}/sql/ls4-codecademy-sql-multiple-tables.md`,
     labels: ["topic: sql"],
   },
 ];
