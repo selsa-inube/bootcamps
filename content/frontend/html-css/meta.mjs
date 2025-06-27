2f551c2d16af8589484d3d93b6da2b20aaefb50f
Fernando Cañas <fernando.canas@outlook.com>
2025-06-27 17:21:34 -0500

    feat(html): refactor meta file

---
+++ content/frontend/html-css/meta.mjs
@@ -0,0 +1,275 @@
+const URL =
+  "https://github.com/selsa-inube/bootcamp/blob/main/content/frontend/html-css";
+
+const lectures = [
+  {
+    title: "Introduction to HTML and CSS",
+    body: `${URL}/html-css-lec-intro-to-html-css.md`,
+    labels: ["lecture", "html-css"],
+  },
+  {
+    title: "Basic Web Pages",
+    body: `${URL}/html-css-lec-basic-web-pages.md`,
+    labels: ["lecture", "html-css"],
+  },
+  {
+    title: "Links and Images",
+    body: `${URL}/html-css-lec-links-and-images.md`,
+    labels: ["lecture", "html-css"],
+  },
+  {
+    title: "Hello, CSS",
+    body: `${URL}/html-css-lec-hello-css.md`,
+    labels: ["lecture", "html-css"],
+  },
+  {
+    title: "CSS Box Model",
+    body: `${URL}/html-css-lec-css-box-model.md`,
+    labels: ["lecture", "html-css"],
+  },
+  {
+    title: "CSS Selectors",
+    body: `${URL}/html-css-lec-css-selectors.md`,
+    labels: ["lecture", "html-css"],
+  },
+  {
+    title: "Pseudoelements and Pseudoclasses",
+    body: `${URL}/html-css-lec-pseudoelements-pseudoclasses.md`,
+    labels: ["lecture", "html-css"],
+  },
+  {
+    title: "Sizing Units",
+    body: `${URL}/html-css-lec-sizing-units.md`,
+    labels: ["lecture", "html-css"],
+  },
+  {
+    title: "Specificity",
+    body: `${URL}/html-css-lec-specificity.md`,
+    labels: ["lecture", "html-css"],
+  },
+  {
+    title: "Floats",
+    body: `${URL}/html-css-lec-floats.md`,
+    labels: ["lecture", "html-css"],
+  },
+  {
+    title: "Flexbox",
+    body: `${URL}/html-css-lec-flexbox.md`,
+    labels: ["lecture", "html-css"],
+  },
+  {
+    title: "Grid",
+    body: `${URL}/html-css-lec-grid.md`,
+    labels: ["lecture", "html-css"],
+  },
+  {
+    title: "Positioning",
+    body: `${URL}/html-css-lec-positioning.md`,
+    labels: ["lecture", "html-css"],
+  },
+  {
+    title: "Z index and Stacking Context",
+    body: `${URL}/html-css-lec-z-index-and-stacking-context.md`,
+    labels: ["lecture", "html-css"],
+  },
+  {
+    title: "Responsive Design",
+    body: `${URL}/html-css-lec-responsive-design.md`,
+    labels: ["lecture", "html-css"],
+  },
+  {
+    title: "Responsive Images",
+    body: `${URL}/html-css-lec-responsive-images.md`,
+    labels: ["lecture", "html-css"],
+  },
+  {
+    title: "Semantic HTML",
+    body: `${URL}/html-css-lec-semantic-html.md`,
+    labels: ["lecture", "html-css"],
+  },
+  {
+    title: "Forms",
+    body: `${URL}/html-css-lec-forms.md`,
+    labels: ["lecture", "html-css"],
+  },
+  {
+    title: "Typography",
+    body: `${URL}/html-css-lec-typography.md`,
+    labels: ["lecture", "html-css"],
+  },
+  {
+    title: "Critical Rendering Path",
+    body: `${URL}/html-css-lec-critical-rendering-path.md`,
+    labels: ["lecture", "html-css"],
+  },
+  {
+    title: "Browsers and Engines",
+    body: `${URL}/html-css-lec-browsers-and-engines.md`,
+    labels: ["lecture", "html-css"],
+  },
+];
+
+const assignments = [
+  {
+    title: "Markup a readme",
+    body: `${URL}/html-css-as-markup-a-readme.md`,
+    labels: ["assignment", "html-css"],
+  },
+  {
+    title: "Press Release",
+    body: `${URL}/html-css-as-press-release.md`,
+    labels: ["assignment", "html-css"],
+  },
+  {
+    title: "Pseudoclasses and Pseudoelements",
+    body: `${URL}/html-css-as-pseudoclasses-pseudoelements.md`,
+    labels: ["assignment", "html-css"],
+  },
+  {
+    title: "Selectors without Classes and Ids",
+    body: `${URL}/html-css-as-selectors-without-classes-and-ids.md`,
+    labels: ["assignment", "html-css"],
+  },
+  {
+    title: "Float Layout",
+    body: `${URL}/html-css-as-float-layout.md`,
+    labels: ["assignment", "html-css"],
+  },
+  {
+    title: "Sidebar",
+    body: `${URL}/html-css-as-sidebar.md`,
+    labels: ["assignment", "html-css"],
+  },
+  {
+    title: "Calendar",
+    body: `${URL}/html-css-as-calendar.md`,
+    labels: ["assignment", "html-css"],
+  },
+  {
+    title: "Landing Page",
+    body: `${URL}/html-css-as-landing-page.md`,
+    labels: ["assignment", "html-css"],
+  },
+  {
+    title: "Stella Studio",
+    body: `${URL}/html-css-as-stella-studio.md`,
+    labels: ["assignment", "html-css"],
+  },
+  {
+    title: "Google",
+    body: `${URL}/html-css-as-google.md`,
+    labels: ["assignment", "html-css"],
+  },
+  {
+    title: "Agency",
+    body: `${URL}/html-css-as-agency.md`,
+    labels: ["assignment", "html-css"],
+  },
+  {
+    title: "Netflix",
+    body: `${URL}/html-css-as-netflix.md`,
+    labels: ["assignment", "html-css"],
+  },
+  {
+    title: "New York Times",
+    body: `${URL}/html-css-as-new-york-times.md`,
+    labels: ["assignment", "html-css"],
+  },
+  {
+    title: "Weather",
+    body: `${URL}/html-css-as-weather.md`,
+    labels: ["assignment", "html-css"],
+  },
+];
+
+const exercises = [
+  {
+    title: "Freecodecamp - Basic HTML and HTML5",
+    body: `${URL}/html-css-ex-freecodecamp-basic-html.md`,
+    labels: ["exercise", "html-css"],
+  },
+  {
+    title: "Freecodecamp - Basic CSS",
+    body: `${URL}/html-css-ex-freecodecamp-basic-css.md`,
+    labels: ["exercise", "html-css"],
+  },
+  {
+    title: "Freecodecamp - Cat Photo App",
+    body: `${URL}/html-css-ex-freecodecamp-cat-photo-app.md`,
+    labels: ["exercise", "html-css"],
+  },
+  {
+    title: "Freecodecamp - Cafe Menu",
+    body: `${URL}/html-css-ex-freecodecamp-cafe-menu.md`,
+    labels: ["exercise", "html-css"],
+  },
+  {
+    title: "Freecodecamp - CSS Flexbox",
+    body: `${URL}/html-css-ex-freecodecamp-css-flexbox.md`,
+    labels: ["exercise", "html-css"],
+  },
+  {
+    title: "Freecodecamp - Photo Gallery",
+    body: `${URL}/html-css-ex-freecodecamp-photo-gallery.md`,
+    labels: ["exercise", "html-css"],
+  },
+  {
+    title: "Freecodecamp - CSS Grid",
+    body: `${URL}/html-css-ex-freecodecamp-css-grid.md`,
+    labels: ["exercise", "html-css"],
+  },
+  {
+    title: "Freecodecamp - Magazine",
+    body: `${URL}/html-css-ex-freecodecamp-magazine.md`,
+    labels: ["exercise", "html-css"],
+  },
+  {
+    title: "Freecodecamp - Rothko Painting",
+    body: `${URL}/html-css-ex-freecodecamp-rothko-painting.md`,
+    labels: ["exercise", "html-css"],
+  },
+  {
+    title: "Freecodecamp - Balance Sheet",
+    body: `${URL}/html-css-ex-frecodecamp-balance-sheet.md`,
+    labels: ["exercise", "html-css"],
+  },
+  {
+    title: "Grid Garden",
+    body: `${URL}/html-css-ex-grid-garden.md`,
+    labels: ["exercise", "html-css"],
+  },
+  {
+    title: "Freecodecamp - Responsive Web Design",
+    body: `${URL}/html-css-ex-freecodecamp-responsive-web-design.md`,
+    labels: ["exercise", "html-css"],
+  },
+  {
+    title: "Freecodecamp - Piano",
+    body: `${URL}/html-css-ex-freecodecamp-piano.md`,
+    labels: ["exercise", "html-css"],
+  },
+  {
+    title: "Freecodecamp - Technical Documentation Page",
+    body: `${URL}/html-css-ex-freecodecamp-technical-doc-page.md`,
+    labels: ["exercise", "html-css"],
+  },
+  {
+    title: "Freecodecamp - Registration Form",
+    body: `${URL}/html-css-ex-freecodecamp-registration-form.md`,
+    labels: ["exercise", "html-css"],
+  },
+  {
+    title: "Freecodecamp - Survey Form",
+    body: `${URL}/html-css-ex-freecodecamp-survey-form.md`,
+    labels: ["exercise", "html-css"],
+  },
+  {
+    title: "Freecodecamp - Nutrition Label",
+    body: `${URL}/html-css-ex-freecodecamp-nutrition-label.md`,
+    labels: ["exercise", "html-css"],
+  },
+];
+
+const htmlCSS = [...lectures, ...assignments, ...exercises];
+
+export { htmlCSS };
